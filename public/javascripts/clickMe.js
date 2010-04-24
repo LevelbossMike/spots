ba8791cd807a6804b6b5d612cb5bfe70dda44ee2
Michael Klein <michael@firstiwaslike.com>
2010-04-24 14:07:05 +0200

    Added spot partial and cleaned up spot#index-view

---
+++ public/javascripts/clickMe.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" charset="utf-8">
+	 $('#Submit').click(function() {
+	  $('#message').html('<br />I told you it was not working!');
+	});
+	$('#clickme').click(function() {
+		$('#spotsDetail').hide('slow')
+	});
+</script>
