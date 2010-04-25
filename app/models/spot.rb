c2dc19eaa590556efcf515a49b77d43813483793
Michael Klein <michael@firstiwaslike.com>
2010-04-25 16:15:14 +0200

    spots#index has now the desired layout and displays 4 spots horizontally.

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,9 +1,9 @@
 class Spot < ActiveRecord::Base
   # pagination
   cattr_reader :per_page
-  @@per_page = 3
+  @@per_page = 4
   validates_presence_of :name,:description,:lat,:lng, :on => :create, :message => "can't be blank"
   has_many :comments, :dependent => :destroy
   # Paperclip
-  has_attached_file :photo, :styles => {:small => "300x300>", :medium => "500x500>"}
+  has_attached_file :photo, :styles => {:small => "300x300>", :medium => "250x200!"}
 end
