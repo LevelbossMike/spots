9daf5980bd7eb68116c5bd66fc097d99ececdf4c
Michael Klein <michael@firstiwaslike.com>
2010-07-13 15:18:18 +0200

    A spot's rating is now displayed with stars

--- public/javascripts/spoiler.js
+++ public/javascripts/spoiler.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
 	$("#stars-rating").stars({
 	    inputType: "select"
 	});
+	$("#avg").stars({
+		inputType: "select",
+		disabled: true
+	});
+	$("#avg").stars("select", Math.round(rating));
 	$('#commentForm').hide();
   	$("#commentButton").toggle(function(){
     	$("#commentForm").show('slow');
